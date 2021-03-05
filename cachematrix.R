48a047ba7d84a299080a6645bc2347d9a08a52c2
Baudert, Erik Merlin <erikmerlin.baudert@de.bosch.com>
2021-03-05 09:38:32 +0100

    Programming Assignment 2: Lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## With this functions you will be able to create a matrix and cache its inverse, so it only needs to be inverted once for every unchanged matrix
 
-## Write a short comment describing this function
+## With this function you create and modify the matrix. The same object stores the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(m) {
+    x <<- m
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) i <<- inv
+  getInverse <- function() i
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## this function checks whether the given matrix already has a cached inverse and calculates it otherwise
 
 cacheSolve <- function(x, ...) {
+  i <- x$getInverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
         ## Return a matrix that is the inverse of 'x'
 }
